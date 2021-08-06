bbe1094e884f50e37fb9f2a16024ec9acdc92ffd
hy <81627289+ip-hy1014@users.noreply.github.com>
2021-08-06 17:16:53 +0900

    Update ABC145C.py

--- ABC145C.py
+++ ABC145C.py
@@ -6,3 +6,12 @@ for x1,y1 in xy:
   for x2,y2 in xy:
     ans += math.sqrt((x1-x2)**2 + (y1-y2)**2)
 print(ans/n)
+
+#別解
+n = int(input())
+xy = [list(map(int,input().split())) for _ in range(n)]
+ans = 0
+for i in range(n):
+  for j in range(n):
+    ans += ((xy[i][0]-xy[j][0])**2 + (xy[i][1]-xy[j][1])**2)**0.5
+print(ans/n)
