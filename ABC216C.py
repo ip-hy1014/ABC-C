f72ad12459f3a6a7f396b5a46f3aeaf511d39078
hy <81627289+ip-hy1014@users.noreply.github.com>
2021-08-31 00:44:31 +0900

    Update ABC216C.py

--- ABC216C.py
+++ ABC216C.py
@@ -2,3 +2,16 @@
 n = int(input())
 ans = bin(n)[2:]
 print(ans.replace("1","BA").replace("0","B"))
+
+#別解
+#nが0になるまでループさせる
+n = int(input())
+ans = ""
+while n > 0:
+  if n % 2 == 0:
+    n = n // 2
+    ans = "B" + ans
+  else:
+    n -= 1
+    ans = "A" + ans
+print(ans)
