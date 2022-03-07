54a51efad45a39281a9c80db81677a4f48e6a20c
hy <81627289+ip-hy1014@users.noreply.github.com>
2022-03-08 01:13:58 +0900

    Add files via upload

---
+++ ABC242C.py
@@ -0,0 +1,9 @@
+n = int(input())
+mod = 998244353
+dp = [[0]*11 for _ in range(n+1)]
+for i in range(1,10):
+  dp[1][i] = 1
+for i in range(1,n):
+  for j in range(1,10):
+    dp[i+1][j] = (dp[i][j-1] + dp[i][j] + dp[i][j+1]) % mod
+print(sum(dp[n])%mod)
