d56ee8b36da56ad63f2af59e274a18171ca6562c
hy <81627289+ip-hy1014@users.noreply.github.com>
2022-03-09 00:11:13 +0900

    Update ABC242C.py

--- ABC242C.py
+++ ABC242C.py
@@ -1,6 +1,6 @@
 n = int(input())
 mod = 998244353
-dp = [[0]*11 for _ in range(n+1)]
+dp = [[0]*11 for _ in range(n+1)] #二次元目の長さを11にすることで、j=1,9の時の場合分けをなくす
 for i in range(1,10):
   dp[1][i] = 1
 for i in range(1,n):
