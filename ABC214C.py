94a7206a6cbbe0d4edbc0b74f20897cf6dd746a5
hy <81627289+ip-hy1014@users.noreply.github.com>
2022-03-11 01:21:25 +0900

    Update ABC214C.py

--- ABC214C.py
+++ ABC214C.py
@@ -1,9 +1,7 @@
 n = int(input())
 s = list(map(int,input().split()))
 t = list(map(int,input().split()))
-INF = 10**10
-ans = [INF]*n
 for i in range(2*n):
-  ans[(i+1)%n] = min(ans[i%n]+s[i%n],t[(i+1)%n])
-for i in ans:
-  print(i)
+  t[(i+1)%n] = min(t[i%n]+s[i%n],t[(i+1)%n])
+for ans in t:
+  print(ans)
