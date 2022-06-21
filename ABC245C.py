9d66480f7d1945cb4c5fb938668100a4d7296a7d
hy <81627289+ip-hy1014@users.noreply.github.com>
2022-06-21 20:30:54 +0900

    Update ABC245C.py

--- ABC245C.py
+++ ABC245C.py
@@ -1,7 +1,9 @@
+#考え方としてはDPを用いる
+#あくまで隣接する要素のみを見れば良いため、2つ前の要素はどうでも良い
 n,k = map(int,input().split())
 a = list(map(int,input().split()))
 b = list(map(int,input().split()))
-c = [a[0],b[0]]
+c = [a[0],b[0]] #隣接する要素が条件を満たすかを記録するための配列,最初はaかbのどちらかの先頭の要素と比較するところからスタート
 for i in range(n):
   s = set()
   for x in c:
@@ -9,5 +11,5 @@ for i in range(n):
       s.add(a[i])
     if abs(x-b[i])<=k:
       s.add(b[i])
-  c = s
+  c = s #ここで次の要素と比較するための要素を記録する
 print("Yes" if c else "No")
