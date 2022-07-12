11cb3c44a51e15bf11319bce5ba3ace831a78de7
hy <81627289+ip-hy1014@users.noreply.github.com>
2022-07-13 00:42:29 +0900

    Update ABC029C.py

--- ABC029C.py
+++ ABC029C.py
@@ -1,4 +1,9 @@
 import itertools
 n = int(input())
 for ans in itertools.product("abc",repeat=n):
-  print("".join(map(str,ans)))
+  print("".join(ans))
+
+#golf
+from itertools import*
+for i in product("abc",repeat=int(input())):
+  print("".join(i))
